a74acb5431cec23efeaffec2b5c0af5a72e20b05
Neil DCruz <neildcruz@Neils-MacBook-Pro.local>
2017-04-06 22:10:31 -0700

    Updated code

--- api-gateway/src/main/java/com/example/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -2,9 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
+@EnableOAuth2Sso
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
