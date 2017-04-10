1090bc21a0a194c4f4af70208599851f7ef9d54c
Neil DCruz <neildcruz@Neils-MacBook-Pro.local>
2017-04-09 21:29:05 -0700

    Added JWT support to Resource server

--- resource-server/src/main/java/com/example/controller/ProtectedResourceController.java
+++ resource-server/src/main/java/com/example/controller/ProtectedResourceController.java
@@ -3,6 +3,8 @@
  */
 package com.example.controller;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class ProtectedResourceController {
 	@GetMapping("/resource")
 	public String protectedResource() {
 		return "This is a protected resource";
+	}
+	
+	@GetMapping("/principal")
+	public Principal principal(Principal p) {
+		return p;
 	}
 	
 
