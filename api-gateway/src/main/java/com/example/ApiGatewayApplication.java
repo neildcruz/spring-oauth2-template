9a44695e5708af5bdc29f36291977a3dcdf2d9d8
Neil DCruz <Neil.DCruz@t-mobile.com>
2017-04-14 15:17:46 -0700

    Refresh scope for api gateway

--- api-gateway/src/main/java/com/example/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@RefreshScope
 @EnableOAuth2Sso
 @EnableDiscoveryClient
 @EnableZuulProxy
